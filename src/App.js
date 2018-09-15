edd79ecb6f57b1d8337e95bb24ed8a7bafc7920d
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2018-09-15 18:59:32 +0300

    change to hash history

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import PokemonApp from "./PokemonApp";
 import PokemonDetails from "./PokemonDetails";
 import "./App.css";
